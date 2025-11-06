ed0da930284de10ba570052f565170fc35beb70f
rehansheikh000004-lab <rehansheikh000004@gmail.com>
2025-11-06 19:18:58 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
-});
+}, { timestamps: true });
 
-export default mongoose.model("User", userSchema);
+// Avoid "OverwriteModelError" in dev/reloads
+export default mongoose.models.User || mongoose.model("User", userSchema);
